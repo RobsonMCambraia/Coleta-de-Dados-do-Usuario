ee8258341c6fc872c1be06d38530df6395642bba
robson-m-cambraia <robson.mcambraia23@gmail.com>
2023-05-02 14:47:50 -0300

    #

---
+++ cadastro.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, name, birth_day, gender, work, career, remuneration, marital_status, ):
+        self.name = name
+        self.birth_day = birth_day
+        self.gender = gender
+        self.work = work
+        self.career = career
+        self.remuneration = remuneration
+        self.marital_status = marital_status
