e21e3133dd741dca9a9f11a6f9e063886bd76c85
robson-m-cambraia <robson.mcambraia23@gmail.com>
2023-05-06 16:02:41 -0300

    Set nome

--- cadastro.py
+++ cadastro.py
@@ -1,41 +1,49 @@
 class User:
-    def __init__(self, name, birth_day, gender, career, remuneration, marital_status):
-        self.name = name
-        self.birth_day = birth_day
-        self.gender = gender
-        self.career = career
-        self.remuneration = remuneration
-        self.marital_status = marital_status
+    def __init__(self):
+        self.name = []
+        self.birth_day = []
+        self.gender = []
+        self.career = []
+        self.remuneration = []
+        self.marital_status = []
         
     def get_name(self):
-        return self._name
+        return self.name
     def set_name(self, name):
-        self._name = name
-
+        while True:
+            name = input('Digite seu nome completo:\t').upper().lstrip(' ')
+            if len(name) > 3:
+                self.name.append(name)
+                break
+            else:
+                print('Insira um nome com mais de 3 caracteres!')
+        
+        print(self.name)
+        
     def get_birth_day(self):
-        return self._birth_day
+        return self.birth_day
     def set_birth_day(self, birth_day):
-        self._birth_day = birth_day
+        self.birth_day = birth_day
 
     def get_gender(self):
-        return self._gender
+        return self.gender
     def set_gender(self, gender):
-        self._gender = gender
+        self.gender = gender
 
     def get_career(self):
-        return self._career
+        return self.career
     def set_career(self, career):
-        self._career = career
+        self.career = career
 
     def get_remuneration(self):
-        return self._remuneration
+        return self.remuneration
     def set_remuneration(self, remuneration):
-        self._remuneration = remuneration
+        self.remuneration = remuneration
 
     def get_marital_status(self):
-        return self._marital_status
+        return self.marital_status
     def set_marital_status(self, marital_status):
-        self._marital_status = marital_status
+        self.marital_status = marital_status
 
         
     def getUsers (self):
---
+++ main.py
@@ -0,0 +1,6 @@
+from cadastro import User
+
+nome = User()
+
+for i in range(0,3):
+    nome.set_name("robson")
